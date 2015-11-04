2e7dc2b8cd499d534759a1eefb3636c8e5bb8d0a
ALRW <awerner1@googlemail.com>
2015-11-04 12:03:03 +0000

    differentiated for test and dev environments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@ require 'database_cleaner'
 
 RSpec.configure do |config|
 
-    config.before(:suite) do
+  config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
