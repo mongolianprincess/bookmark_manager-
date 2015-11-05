533b7830e3342f78db130508b514d64922ff6db7
Roi Driscoll <roi.driscoll@gmail.com>
2015-11-05 19:49:55 +0000

    ch_17 new feature and refactor

--- app/app.rb
+++ app/app.rb
@@ -2,9 +2,7 @@ ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
 require './app/data_mapper_setup.rb'
-
 require_relative 'models/link'
-
 
 class BookmarkManager < Sinatra::Base
   get '/' do
@@ -20,22 +18,26 @@ class BookmarkManager < Sinatra::Base
     erb(:'links/new_link')
   end
 
+  get '/links/new' do
+    erb(:'links/new_link')
+  end
+
   post '/links/new/add' do
-    @tag = Tag.create(name: params[:name])
-    Link.find do |link|
-      if link.title == params[:title]
-        @existing_link = link
-      end
-    end
-    if @existing_link != nil
-      @existing_link.tags << @tag
-      @existing_link.save
+    Link.select {|link| @old_link = link if link.title == params[:title]}
+
+    Tag.select {|tag| @old_tag = tag if tag.name == params[:name]}
+
+    if @old_link != nil && @old_tag != nil
+      (@old_link.tags << @old_tag).save
+    elsif @old_link != nil && @old_tag == nil
+      (@old_link.tags << Tag.create(name: params[:name])).save
+    elsif @old_link == nil && @old_tag != nil
+      new_link = Link.create(title: params[:title], url: params[:url])
+      (new_link.tags << @old_tag).save
     else
         new_link = Link.create(title: params[:title], url: params[:url])
-        new_link.tags << @tag
-        new_link.save
+        (new_link.tags << Tag.create(name: params[:name])).save
     end
-
     redirect :links
   end
 
