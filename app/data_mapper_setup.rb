ceb671408692b60bce32a1ba1d3a41b3fb9baf35
Roi Driscoll <roi.driscoll@gmail.com>
2015-11-06 11:09:28 +0000

    erase everything

--- app/data_mapper_setup.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'models/link.rb'
-require_relative 'models/tag.rb'
-require_relative 'models/user'
-
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-
-DataMapper.finalize
